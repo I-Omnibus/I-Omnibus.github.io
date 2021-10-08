97569b05a72605133a2b0ff1e1c4019df6b42d27
Omnibus <68746849+I-Omnibus@users.noreply.github.com>
2021-10-08 18:50:27 +0100

    Update Dependencies.js

--- Dependencies.js
+++ Dependencies.js
@@ -1,9 +1,38 @@
-class Dependencies{
-  static load(){
-    console.log("Scripts");
+class Dependencies {
+  static async resolve(scripts = []) {
+    const asyncScriptLoader = [];
+
+    scripts.forEach((name) => {
+      const scriptUrl = `${name}.js${Dependencies.bypassCache(false)}`;
+      asyncScriptLoader.push(Dependencies.scriptLoadPromise(scriptUrl));
+    });
+
+    await Promise.all(asyncScriptLoader).catch((scriptUrl) => {
+      console.error(`Script load failure: ${scriptUrl}`);
+    });
+
+    return;
+  }
+
+  static scriptLoadPromise(scriptUrl) {
+    return new Promise((resolve, reject) => {
+      Dependencies.load(scriptUrl, resolve, reject);
+    });
+  }
+
+  static load(scriptUrl, resolve, reject) {
+    const tag = document.createElement("script");
+    //tag.type = "module";
+    tag.onload = () => resolve(scriptUrl);
+    tag.onerror = (error) => reject(error.target.src);
+
+    document.head.appendChild(tag);
+    tag.src = scriptUrl;
+  }
+
+  static bypassCache(enabled = true) {
+    return enabled ? "?" + (Math.floor(Math.random() * 9000) + 1000) : "";
   }
 }
-
-
 
 export {Dependencies};
